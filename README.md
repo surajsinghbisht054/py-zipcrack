893864b9a32965033c3e2cfd1ea75e83139abb88
Suraj Singh Bisht <surajsinghbisht054@gmail.com>
2017-01-06 14:50:58 +0530

    Latest Update

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # pyzipcrack
 # python Script For Zip File password recovery
 
+# This Script Is Created For http://bitforestinfo.blogspot.in
+# This Script is Written By
+__author__='''
 
 ######################################################
-			By SSB Group							
+                By S.S.B Group                          
 ######################################################
 
-	Suraj Singh
-	Admin
-	SSB Group
-	surajsinghbisht054@gmail.com
-	https://hackworldwithssb.blogspot.in
+    Suraj Singh
+    Admin
+    S.S.B Group
+    surajsinghbisht054@gmail.com
+    http://bitforestinfo.blogspot.in/
 
-	Note: We Feel Proud To Be Indian
+    Note: We Feel Proud To Be Indian
 ######################################################
-
+'''
+print __author__
 ******************* Features ************************
 
 This Script Supports Only Zip File in This Verson
--- pyzipcrack/pyzipcrack.py
+++ pyzipcrack/pyzipcrack.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
 
-# =========================================================================|
-#   This Script is Created Only for Practise And Educational Purpose Only
-# =========================================================================|
+# ---------------- READ ME ---------------------------------------------
+# This Script is Created Only For Practise And Educational Purpose Only
+# This Script Is Created For http://bitforestinfo.blogspot.in
+# This Script is Written By
 __author__='''
+
 ######################################################
-			By S.S.B Group							
+                By S.S.B Group                          
 ######################################################
 
-	Suraj Singh
-	Admin
-	S.S.B Group
-	surajsinghbisht054@gmail.com
-	https://hackworldwithssb.blogspot.in
+    Suraj Singh
+    Admin
+    S.S.B Group
+    surajsinghbisht054@gmail.com
+    http://bitforestinfo.blogspot.in/
 
-	Note: We Feel Proud To Be Indian
+    Note: We Feel Proud To Be Indian
 ######################################################
 '''
 # =================Other Configuration================ 
