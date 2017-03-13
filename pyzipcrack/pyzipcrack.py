26063e3e66aa974adad44cf0dc47c106dc93a40f
Suraj Singh Bisht <surajsinghbisht054@gmail.com>
2017-03-13 22:58:08 +0530

    Latest Update

--- pyzipcrack/pyzipcrack.py
+++ pyzipcrack/pyzipcrack.py
@@ -4,6 +4,14 @@
 # This Script is Created Only For Practise And Educational Purpose Only
 # This Script Is Created For http://bitforestinfo.blogspot.in
 # This Script is Written By
+#
+#
+##################################################
+######## Please Don't Remove Author Name #########
+############### Thanks ###########################
+##################################################
+#
+#
 __author__='''
 
 ######################################################
